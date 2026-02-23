d82c4d3c3be148bf5747aadeb2534ecdef90f1f9
JimIITDU <151479893+JimIITDU@users.noreply.github.com>
2026-02-23 06:31:08 +0600

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,19 @@
 require('dotenv').config();
 
-const config = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'newsportal',
-  host: process.env.DB_HOST || 'localhost',
-  dialect: 'mysql',
-  dialectOptions: { ssl: false }
-};
-
 module.exports = {
-  development: config,
-  production: config
+  development: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'newsportal',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'railway',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'mysql',
+    dialectOptions: { ssl: false }
+  }
 };
