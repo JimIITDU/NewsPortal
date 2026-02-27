d537a29d72303565f61f35f5a0bd6ef7924c1af8
JimIIT <akidul201103@gmail.com>
2026-02-27 14:33:31 +0600

    file mismatch fixed 2

--- backend/routes/reporterApplicationRoutes.js
+++ backend/routes/reporterApplicationRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/ReporterApplicationController');
+const controller = require('../controllers/reporterApplicationController');
 const authMiddleware = require('../middlewares/authMiddleware');
 const adminMiddleware = require('../middlewares/adminMiddleware');
 
